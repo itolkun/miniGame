209a300e79626eb918da8e4def1cfa4de28d9cb4
Aitolkun <totita@list.ru>
2022-12-20 18:32:11 +0300

    Initial Commit

---
+++ miniGame/miniGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniGameApp.swift
+//  miniGame
+//
+//  Created by Айтолкун Анарбекова on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct miniGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
